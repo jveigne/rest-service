553136b87499d36b004a2860a97085a15608d3c0
jveigne <58737612+jveigne@users.noreply.github.com>
2021-06-10 01:10:15 +0200

    Just a test to use git extension

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -10,6 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+
     
     private final AtomicLong counter = new AtomicLong();
 
